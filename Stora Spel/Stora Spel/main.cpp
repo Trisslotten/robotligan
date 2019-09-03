cf24e5f1fda9058ccc5c5140952e7b9e25b1ea0f
Jesper N <Jesper N@DESKTOP-QCH8PKF>
2019-09-03 11:29:58 +0200

    Code review fixed

--- Stora Spel/Stora Spel/main.cpp
+++ Stora Spel/Stora Spel/main.cpp
@@ -5,7 +5,7 @@ int main(unsigned argc, char **argv)
 {
 	std::cout << "Hello World!*!!!111\n";
 
-	std::cout << "Test från development" << std::endl;
+	std::cout << "Test från development";
 
 	return EXIT_SUCCESS;
 }
