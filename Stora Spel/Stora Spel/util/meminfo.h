bb546ab600af5fd2ce0fe37ed8ad7e38cbcefeb6
Patrik Sjölund <pasf15@student.bth.se>
2019-09-10 14:21:14 +0200

    Fixade variabelnamn

--- Stora Spel/Stora Spel/util/meminfo.h
+++ Stora Spel/Stora Spel/util/meminfo.h
@@ -24,8 +24,8 @@ class MemoryInfo {
     return instance;
   }
   unsigned GetUsedRAM(unsigned short suffix = MB) {
-    GetProcessMemoryInfo(GetCurrentProcess(), &PMC_, sizeof(PMC_));
-    return (unsigned)(PMC_.WorkingSetSize >> suffix);
+    GetProcessMemoryInfo(GetCurrentProcess(), &pmc_, sizeof(pmc_));
+    return (unsigned)(pmc_.WorkingSetSize >> suffix);
   }
   /*
 	Returnerar hur mycket VRAM processen använder sig utav
@@ -34,22 +34,22 @@ class MemoryInfo {
   int GetUsedVRAM(unsigned short suffix = MB) {
 	  //Om funktionen kallas mer än en gång så måste vi släppa på föregående 
 	  //Devices för att undvika memoryleaks.
-    if (Curr_Adapter_ || Adapters_) {
-      Dx_factory_->Release();
-      Adapters_->Release();
+    if (curr_adapter_ || adapters_) {
+      dx_factory_->Release();
+      adapters_->Release();
     }
 	//Adapters innehåller alla renderings-enheter, både fysiska och virtuella 
-    Curr_Adapter_ = nullptr;
-    Adapters_ = nullptr;
+    curr_adapter_ = nullptr;
+    adapters_ = nullptr;
 	//Vi vet att vi enbart vill ha den första fysiska enheten, så vi skippar att loopa
-    Dx_factory_->EnumAdapters1(0, &Curr_Adapter_);
+    dx_factory_->EnumAdapters1(0, &curr_adapter_);
     DXGI_ADAPTER_DESC1 desc;
-    Curr_Adapter_->GetDesc1(&desc);
-    if (!Adapters_ && desc.Flags == 0) {
-      Curr_Adapter_->QueryInterface(IID_PPV_ARGS(&Adapters_));
+    curr_adapter_->GetDesc1(&desc);
+    if (!adapters_ && desc.Flags == 0) {
+      curr_adapter_->QueryInterface(IID_PPV_ARGS(&adapters_));
 	  //Få fram minnesinfo från adaptern.
       DXGI_QUERY_VIDEO_MEMORY_INFO info = {};
-      Adapters_->QueryVideoMemoryInfo(0, DXGI_MEMORY_SEGMENT_GROUP_LOCAL,
+      adapters_->QueryVideoMemoryInfo(0, DXGI_MEMORY_SEGMENT_GROUP_LOCAL,
                                       &info);
 	  //Returnera mängden använt minne eller -1 om det går fel.
       return (int)(info.CurrentUsage >> suffix);
@@ -61,19 +61,19 @@ class MemoryInfo {
 	Samma princip som ovan, fast returnera hur mycket minne som finns på enheten
   */
   int GetVRAMAmount(unsigned short suffix = MB) {
-    if (Curr_Adapter_ || Adapters_) {
-      Dx_factory_->Release();
-      Adapters_->Release();
+    if (curr_adapter_ || adapters_) {
+      dx_factory_->Release();
+      adapters_->Release();
     }
-    Curr_Adapter_ = nullptr;
-    Adapters_ = nullptr;
-    Dx_factory_->EnumAdapters1(0, &Curr_Adapter_);
+    curr_adapter_ = nullptr;
+    adapters_ = nullptr;
+    dx_factory_->EnumAdapters1(0, &curr_adapter_);
     DXGI_ADAPTER_DESC1 desc;
-    Curr_Adapter_->GetDesc1(&desc);
-    if (!Adapters_ && desc.Flags == 0) {
-      Curr_Adapter_->QueryInterface(IID_PPV_ARGS(&Adapters_));
+    curr_adapter_->GetDesc1(&desc);
+    if (!adapters_ && desc.Flags == 0) {
+      curr_adapter_->QueryInterface(IID_PPV_ARGS(&adapters_));
       DXGI_QUERY_VIDEO_MEMORY_INFO info = {};
-      Adapters_->QueryVideoMemoryInfo(0, DXGI_MEMORY_SEGMENT_GROUP_LOCAL,
+      adapters_->QueryVideoMemoryInfo(0, DXGI_MEMORY_SEGMENT_GROUP_LOCAL,
                                       &info);
       return (int)(info.Budget >> suffix);
     } else {
@@ -82,10 +82,10 @@ class MemoryInfo {
   }
 
  private:
-  MemoryInfo() { CreateDXGIFactory1(IID_PPV_ARGS(&Dx_factory_)); }
+  MemoryInfo() { CreateDXGIFactory1(IID_PPV_ARGS(&dx_factory_)); }
   ~MemoryInfo() {
-    Dx_factory_->Release();
-    Adapters_->Release();
+    dx_factory_->Release();
+    adapters_->Release();
   }
   MemoryInfo(const MemoryInfo&) = delete;
   MemoryInfo& operator=(const MemoryInfo&) = delete;
@@ -93,10 +93,10 @@ class MemoryInfo {
   MemoryInfo& operator=(MemoryInfo&&) = delete;
 
   //DX-variabler för GPU information.
-  IDXGIFactory4* Dx_factory_ = nullptr;
-  IDXGIAdapter3* Adapters_ = nullptr;
-  IDXGIAdapter1* Curr_Adapter_ = nullptr;
-  PROCESS_MEMORY_COUNTERS PMC_ = {};
+  IDXGIFactory4* dx_factory_ = nullptr;
+  IDXGIAdapter3* adapters_ = nullptr;
+  IDXGIAdapter1* curr_adapter_ = nullptr;
+  PROCESS_MEMORY_COUNTERS pmc_ = {};
 };
 }  // namespace util
 #endif  // !MEMORYINFO_H
