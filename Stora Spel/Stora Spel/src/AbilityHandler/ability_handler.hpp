2952281c7d0ef3a7b3c63e0f8e9a74aff36ea0c7
SEBASTIAN VUSAK <sevu15@student.bth.se>
2019-09-12 13:39:40 +0200

    Ability handler started

---
+++ Stora Spel/Stora Spel/src/AbilityHandler/ability_handler.hpp
@@ -0,0 +1,16 @@
+#ifndef ABILITY_HANDLER_HPP_
+#define ABILITY_HANDLER_HPP_
+
+enum AbilityID {
+	NULL_ABILITY,
+	//Fill with abilities here
+	NUM_OF_ABILITY_IDS
+};
+
+class AbilityHandler {
+ private:
+	 
+ public:
+};
+
+#endif  // !ABILITY_HANDLER_HPP_
