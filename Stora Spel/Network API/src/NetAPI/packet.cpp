7d6c3cebe1803ddb086b0cc7c1ae79e5a4995674
Patrik Sjölund <pasf15@student.bth.se>
2019-09-17 13:07:09 +0200

    Fix comments

--- Stora Spel/Network API/src/NetAPI/packet.cpp
+++ Stora Spel/Network API/src/NetAPI/packet.cpp
@@ -8,7 +8,7 @@ NetAPI::Common::Packet::Packet(size_t in_size) {
 
 NetAPI::Common::Packet::Packet(const char* in_buffer, size_t in_size) {
   data_ = new char[in_size + sizeof(p_)];
-  memcpy(data_, &p_, sizeof(p_));
+  memcpy(data_, &in_buffer, in_size);
   size_of_data_ = in_size;
 }
 
