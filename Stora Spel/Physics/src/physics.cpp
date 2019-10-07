bb57184741cd60cda5444e937ae95eb09451bcf1
Jesper N <Jesper N@DESKTOP-QCH8PKF>
2019-10-07 16:29:20 +0200

    acceleration i fysiksystemet

--- Stora Spel/Physics/src/physics.cpp
+++ Stora Spel/Physics/src/physics.cpp
@@ -5,13 +5,16 @@
 constexpr float g = 9.82f;
 
 void physics::Update(physics::PhysicsObject* po, float dt) {
-  float deacc = po->friction * dt;
-  float vel = glm::length(po->velocity);
-  vel -= deacc;
   glm::vec3 old_vel = po->velocity;
+  po->velocity += po->acceleration * dt;
+  float vel = glm::length(po->velocity);
+  float deacc = po->friction * dt;
+  vel -= deacc;
   if (vel <= 0) {
     po->velocity = glm::vec3(0);
   } else {
+    if (vel > po->max_speed) vel = po->max_speed;
+
     po->velocity = glm::normalize(po->velocity) * vel;	
   }
 
