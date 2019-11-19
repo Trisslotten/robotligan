fa5e6804fd98ad7b54e2b7306d0d312c82a64a65
felix <fena15@student.bth.se>
2019-11-19 13:25:52 +0100

    fixed standing on player?

--- Stora Spel/Stora Spel/src/states/playstate.cpp
+++ Stora Spel/Stora Spel/src/states/playstate.cpp
@@ -1026,11 +1026,12 @@ void PlayState::Collision() {
 
   auto& my_obb = registry_gameplay_.get<physics::OBB>(my_entity_);
   auto& my_phys_c = registry_gameplay_.get<PhysicsComponent>(my_entity_);
+  auto& my_id = registry_gameplay_.get<IDComponent>(my_entity_);
   auto& arena_hitbox =
       registry_gameplay_.get<physics::MeshHitbox>(arena_entity_);
   auto& arena_hitbox2 =
       registry_gameplay_.get<FailSafeArenaComponent>(arena_entity_);
-
+  //collision with arena
   physics::IntersectData data =
       Intersect(arena_hitbox, my_obb, -my_phys_c.velocity);
   if (data.collision) {
@@ -1087,6 +1088,24 @@ void PlayState::Collision() {
       predicted_state_.velocity.y = 0.f;
       server_predicted_.velocity.y = 0.f;
     }
+  }
+  //collision with player
+  auto view_player =
+      registry_gameplay_.view<PlayerComponent, physics::OBB, IDComponent>();
+  for (auto player : view_player) {
+    auto& id_c = view_player.get<IDComponent>(player);
+    if (id_c.id != my_id.id) {
+      auto& obb = view_player.get<physics::OBB>(player);
+      physics::IntersectData data = physics::Intersect(my_obb, obb);
+      if (data.collision) {
+        my_obb.center += data.move_vector;
+        if (data.move_vector.y > 0) {
+          my_phys_c.velocity.y = 0.0f;
+          predicted_state_.velocity.y = 0.f;
+          server_predicted_.velocity.y = 0.f;
+		}
+	  }
+	}
   }
   // update positions
   auto view_sphere =
@@ -1224,26 +1243,27 @@ void PlayState::CreatePlayerEntities() {
                                               sound_engine.CreatePlayer());
 
 	auto& model_c = registry_gameplay_.assign<ModelComponent>(entity);
+    character_scale = glm::vec3(0.1f);
+    float coeff_x_side = (11.223f - (-0.205f));
+    float coeff_y_side = (8.159f - (-10.316f));
+    float coeff_z_side = (10.206f - (-1.196f));
+
+    registry_gameplay_.assign<physics::OBB>(
+        entity,
+        alter_scale * character_scale,            // Center
+        glm::vec3(1.f, 0.f, 0.f),                 //
+        glm::vec3(0.f, 1.f, 0.f),                 // Normals
+        glm::vec3(0.f, 0.f, 1.f),                 //
+        coeff_x_side * character_scale.x * 0.4f,  //
+        coeff_y_side * character_scale.y * 0.5f,  // Length of each plane
+        coeff_z_side * character_scale.z * 0.7f   //
+    );
 
     if (entity_id == my_id_) {
       glm::vec3 camera_offset = glm::vec3(-0.2f, 0.4f, 0.f);
       registry_gameplay_.assign<CameraComponent>(entity, camera_offset,
                                                  glm::quat(glm::vec3(0.f)));
-      character_scale = glm::vec3(0.1f);
-      float coeff_x_side = (11.223f - (-0.205f));
-      float coeff_y_side = (8.159f - (-10.316f));
-      float coeff_z_side = (10.206f - (-1.196f));
-
-      registry_gameplay_.assign<physics::OBB>(
-          entity,
-          alter_scale * character_scale,            // Center
-          glm::vec3(1.f, 0.f, 0.f),                 //
-          glm::vec3(0.f, 1.f, 0.f),                 // Normals
-          glm::vec3(0.f, 0.f, 1.f),                 //
-          coeff_x_side * character_scale.x * 0.4f,  //
-          coeff_y_side * character_scale.y * 0.5f,  // Length of each plane
-          coeff_z_side * character_scale.z * 0.7f   //
-      );
+     
       model_c.handles.push_back(FPS_model);
 
       registry_gameplay_.assign<AnimationComponent>(
